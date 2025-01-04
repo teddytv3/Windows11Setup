f870391c61c3a3916938a7a9c8bd422953103d5f
Bryan A <90355171+teddytv3@users.noreply.github.com>
2025-01-04 03:16:47 -0500

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -58,27 +58,40 @@ If (!(Test-Path $FullRegKeyName)) {
 
 New-itemproperty $FullRegKeyName -Name $RegValueName -Value "1" -Type STRING -Force
 
-REG LOAD HKLM\Default C:\Users\Default\NTUSER.DAT
+$UserProfiles = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*" |
+Where-Object { $_.PSChildName -match "S-1-5-21-(\d+-?){4}$" } |
+Select-Object @{Name = "SID"; Expression = { $_.PSChildName } }, @{Name = "UserHive"; Expression = { "$($_.ProfileImagePath)\NTuser.dat" } }
 
-# Removes Task View from the Taskbar
-Write-Host "Attempting to run: $PSItem"
-$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value "0" -PropertyType Dword -Force
-try { $reg.Handle.Close() } catch {}
+# Loop through each profile on the machine
+ foreach ($UserProfile in $UserProfiles) {
+    Write-Host "Running for profile: $($UserProfile.UserHive)"
+    # Load User NTUser.dat if it's not already loaded
+    if (($ProfileWasLoaded = Test-Path Registry::HKEY_USERS\$($UserProfile.SID)) -eq $false) {
+        Start-Process -FilePath "CMD.EXE" -ArgumentList "/C REG.EXE LOAD HKU\$($UserProfile.SID) $($UserProfile.UserHive)" -Wait -WindowStyle Hidden
+    }
+    Write-Host "Attempting to run: $PSItem"
+    $reg = New-ItemProperty "registry::HKEY_USERS\$($UserProfile.SID)\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value "0" -PropertyType Dword -Force
+    try { $reg.Handle.Close() } catch {}
 
-# Removes Widgets from the Taskbar
-Write-Host "Attempting to run: $PSItem"
-$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value "0" -PropertyType Dword -Force
-try { $reg.Handle.Close() } catch {}
+    # Removes Widgets from the Taskbar
+    Write-Host "Attempting to run: $PSItem"
+    $reg = New-ItemProperty "registry::HKEY_USERS\$($UserProfile.SID)\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value "0" -PropertyType Dword -Force
+    try { $reg.Handle.Close() } catch {}
+                
+    # Removes Chat from the Taskbar
+    Write-Host "Attempting to run: $PSItem"
+    $reg = New-ItemProperty "registry::HKEY_USERS\$($UserProfile.SID)\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value "0" -PropertyType Dword -Force
+    try { $reg.Handle.Close() } catch {}
 
-# Removes Chat from the Taskbar
-Write-Host "Attempting to run: $PSItem"
-$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value "0" -PropertyType Dword -Force
-try { $reg.Handle.Close() } catch {}
-
-# Default StartMenu alignment 0=Left
-Write-Host "Attempting to run: $PSItem"
-$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value "0" -PropertyType Dword -Force
-try { $reg.Handle.Close() } catch {}    
-
-[GC]::Collect()
-REG UNLOAD HKLM\Default
+    # Default StartMenu alignment 0=Left
+    Write-Host "Attempting to run: $PSItem"
+    $reg = New-ItemProperty "registry::HKEY_USERS\$($UserProfile.SID)\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value "0" -PropertyType Dword -Force
+    try { $reg.Handle.Close() } catch {}
+            
+    # Unload NTUser.dat
+    if ($ProfileWasLoaded -eq $false) {
+        [GC]::Collect()
+        Start-Sleep 1
+        Start-Process -FilePath "CMD.EXE" -ArgumentList "/C REG.EXE UNLOAD HKU\$($UserProfile.SID)" -Wait -WindowStyle Hidden
+    }
+}
